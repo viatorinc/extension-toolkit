8186f73c141ccf8468b0b664d4eab8ceac94b15d
Rijk van Zanten <rijkvanzanten@me.com>
2019-04-01 17:40:47 -0400

    Add initial version of CLI

---
+++ cli/cmds/build.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const Bundler = require("parcel-bundler");
+const path = require("path");
+const jsonminify = require("jsonminify");
+
+exports.command = "build [input] [output]";
+exports.desc = "Build the source files for production use";
+exports.handler = async function(argv) {
+  let { input, output } = argv;
+  input = input || "src";
+  output = output || "dist";
+
+  const entryFiles = path.join(input, "*.vue");
+
+  const bundler = new Bundler(entryFiles, {
+    outDir: output,
+    watch: false,
+    minify: true
+  });
+
+  await bundler.bundle();
+
+  const metaFile = fs.readFileSync(path.join(input, "meta.json"), "utf8");
+  fs.writeFileSync(path.join(output, "meta.json"), jsonminify(metaFile));
+}
---
+++ cli/templates/display.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ displayValue }}
+  </div>
+</template>
+
+<script>
+  import mixin from "@directus/extension-toolkit/mixins/interface";
+
+  export default {
+    mixins: [mixin],
+    computed: {
+      displayValue() {
+        return this.value.toLowerCase();
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+div {
+  background-color: var(--red-300);
+}
+</style>
